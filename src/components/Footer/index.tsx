b0215632695f83e6d67f1e88cfd89b9364d5d039
Snay1 <stpshk767@gmail.com>
2023-02-17 02:49:14 +0300

    footer completed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <footer className={s.footer}>
             <div className={`${s.content} _container`}>
-                <p>© 2023</p>
+                <p>snayi portfolio © 2023</p>
                 <ul className={s.socials}>
                     {
                         socials && socials.map(social => <li key={social.title}><a href={social.link} target='_blank' rel="noreferrer"><IconSelector icon={social.icon} /></a></li>)
